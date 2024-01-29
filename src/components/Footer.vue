aeaf5abf68071d64ab055d4d54e953f5d5d64c40
ArifDoofazit <arif@doofazit.com>
2024-01-29 20:32:19 +0600

    unitech

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,19 +6,19 @@
         <div class="h-96 w-auto" style="background:rgb(39 50 112);">
 
 
-            <div class="grid gap-x-8 gap-y-4 grid-cols-3">
-                    <div class="lg:ml-32 lg:mt-4">
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 gap-x-8 gap-y-4 h-5/6">
+                    <div class="lg:ml-32 lg:mt-4 ml-5">
                         <img src="../assets/Capture.jpg1.PNG" alt="">
                         <img src="../assets/Capture.jpg 2.PNG" alt="">
                     </div>
-                    <div class="lg:ml-20 lg:mt-4">
+                    <div class="lg:ml-20 lg:mt-4 ml-5">
                         <h2 class="text-white text-xl">Contact Us</h2>
                         <h4 class="text-white text-lg" style="color:#eb9843;"> +8801877-040404 </h4>
                         <h5 class="text-white">E: unitechg@gmail.com</h5>
                         <h5 class="text-white lg:mt-7">House 49, Road 07, Nikunja 1 <br>
                         Dhaka 1223, Bangladesh  </h5>
                     </div>
-                    <div class="lg:mt-4">
+                    <div class="lg:mt-4 ml-5">
                         <h2 class="text-white text-xl">Order Now</h2>
                         <p class="text-white text-sm">From anywhere in Bangladesh, the best premium quality produts, <br>
                             order us for any kind of items of hardware products </p>
@@ -33,7 +33,7 @@
                     </div>
             </div>
 
-                <hr class="w-11/12 lg:mt-10 lg:ml-16">
+                <hr class="w-11/12 lg:ml-16">
 
 
         </div>
