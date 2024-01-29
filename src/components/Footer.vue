2d4d8bec8c4518211ddfaf6fea2d1809eaf8a5e8
ArifDoofazit <arif@doofazit.com>
2024-01-29 18:29:57 +0600

    Unitech

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <div class="h-96 w-auto" style="background:rgb(39 50 112);">
+
+
+            <div class="grid gap-x-8 gap-y-4 grid-cols-3">
+                    <div class="lg:ml-32 lg:mt-4">
+                        <img src="../assets/Capture.jpg1.PNG" alt="">
+                        <img src="../assets/Capture.jpg 2.PNG" alt="">
+                    </div>
+                    <div class="lg:ml-20 lg:mt-4">
+                        <h2 class="text-white text-xl">Contact Us</h2>
+                        <h4 class="text-white text-lg" style="color:#eb9843;"> +8801877-040404 </h4>
+                        <h5 class="text-white">E: unitechg@gmail.com</h5>
+                        <h5 class="text-white lg:mt-7">House 49, Road 07, Nikunja 1 <br>
+                        Dhaka 1223, Bangladesh  </h5>
+                    </div>
+                    <div class="lg:mt-4">
+                        <h2 class="text-white text-xl">Order Now</h2>
+                        <p class="text-white text-sm">From anywhere in Bangladesh, the best premium quality produts, <br>
+                            order us for any kind of items of hardware products </p>
+
+                            <div class="lg:mt-32">
+                                <a href="#" class="bg-white lg:ml-4 rounded-lg px-2 py-1 text-sm font-medium lg:h-8 lg:w-7 lg:mt-1" style="color: #2c306b;"> <i class="fa-solid fa-phone"></i> </a>
+                                <a href="#" class="bg-white lg:ml-4 rounded-lg px-2 py-1 text-sm font-medium lg:h-8 lg:w-7 lg:mt-1" style="color: #2c306b;"> <i class="fa-brands fa-facebook-f"></i> </a>
+                                <a href="#" class="bg-white lg:ml-4 rounded-lg px-2 py-1 text-sm font-medium lg:h-8 lg:w-7 lg:mt-1" style="color: #2c306b;"> <i class="fa-brands fa-instagram"></i> </a>
+                                <a href="#" class="bg-white lg:ml-4 rounded-lg px-2 py-1 text-sm font-medium lg:h-8 lg:w-7 lg:mt-1" style="color: #2c306b;"> <i class="fa-brands fa-youtube"></i> </a>
+                            </div>
+
+                    </div>
+            </div>
+
+                <hr class="w-11/12 lg:mt-10 lg:ml-16">
+
+
+        </div>
+    </section>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+</script>
+
+<template>
+<nav class="bg-[#fff]">
+    <div class="relative flex h-16 items-center justify-between">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <!-- Mobile menu button-->
+        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="absolute -inset-0.5"></span>
+          <span class="sr-only">Open main menu</span>
+          <!--
+            Icon when menu is closed.
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+
+          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex flex-shrink-0 items-center">
+          <img class="h-10 lg:w-full lg:ml-5" src="../assets/Capture.jpg" alt="Your Company">
+        </div>
+        <div class="hidden lg:ml-auto lg:w-5/12 sm:block rounded-3xl" style="background:#2c306b;">
+          <div class="flex space-x-3">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium" style="color:#f89942;" aria-current="page">Home</a>
+            <a href="#" class="text-white  rounded-md px-3 py-2 text-sm font-medium">About</a>
+            <a href="#" class="text-white  rounded-md px-3 py-2 text-sm font-medium">Service</a>
+            <a href="#" class="text-white  rounded-md px-3 py-2 text-sm font-medium">Products</a>
+            <a href="#" class="text-white rounded-md px-3 py-2 text-sm font-medium">Gallery</a>
+            <a href="#" class="text-white rounded-md px-3 py-2 text-sm font-medium">Contact us</a>
+
+            <a href="#" class="bg-white rounded-3xl px-2 py-1 text-sm font-medium lg:h-7 lg:w-7 lg:mt-1" style="color: #2c306b;"> <i class="fa-solid fa-phone"></i> </a>
+            <a href="#" class="bg-white rounded-3xl px-2 py-1 text-sm font-medium lg:h-7 lg:w-7 lg:mt-1 " style="color: #2c306b;"> <i class="fa-solid fa-location-dot"></i> </a>
+                      
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="sm:hidden" id="mobile-menu">
+    <div class="space-y-1 px-2 pb-3 pt-2">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+      <a href="#" class="hover:bg-gray-700 block rounded-md px-3 py-2 text-base font-medium" style="color:#f89942;" aria-current="page">Home</a>
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Service</a>
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Products</a>
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Gallery</a>
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact us</a>
+    </div>
+  </div>
+</nav>
+
+</template>
