c08e7e9c757c274c8bfa26114f63c156db3fea33
ArifDoofazit <arif@doofazit.com>
2024-01-30 17:09:10 +0600

    Blogpage-clients

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,7 +1,12 @@
 <script setup>
     import Slider from '@/components/Slider.vue';
+    import Clients from '@/components/Clients.vue';
+    import Blogpost from '@/components/Blogpost.vue';
 </script>
 
 <template>
     <Slider></Slider>
+    <Blogpost></Blogpost>
+    <Clients></Clients>
+    
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,24 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
       colors: {
         yellow: '#eb9843' ,
+      },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(215, 215, 218)',
+        '4xl': [
+            '0 35px 35px rgba(215, 215, 218)',
+            '0 45px 65px rgba(215, 215, 218)'
+        ]
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+]
 }
 
