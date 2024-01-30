3d8ad45be8738dc70a9659f8f05a18ed722d985d
ArifDoofazit <arif@doofazit.com>
2024-01-30 21:10:05 +0600

    banerpage

---
+++ src/components/Banerpage.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="">
+        <div class="">
+            <img src="../assets/banner-hardware.jpg" class="w-full lg:h-60 h-40 mt-10 opacity-30" alt="">
+            <div class="container">
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 gap-x-8 gap-y-4">
+                    <div class=""></div>
+                    <div class=""></div>
+                    <div class=""></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -2,10 +2,12 @@
     import Slider from '@/components/Slider.vue';
     import Clients from '@/components/Clients.vue';
     import Blogpost from '@/components/Blogpost.vue';
+    import Banerpage from '@/components/Banerpage.vue';
 </script>
 
 <template>
     <Slider></Slider>
+    <Banerpage></Banerpage>
     <Blogpost></Blogpost>
     <Clients></Clients>
     
