f91efa58625502e9b450d1daef9f38016e56dd83
ShFarhan767 <farhan@doofazit.com>
2024-01-31 15:33:15 +0600

    problem

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,36 +1,5 @@
 <script>
-export default {
-    mounted() {
-        this.setupSlider();
-    },
-    methods: {
-        setupSlider() {
-            const cardsContainer = this.$el.querySelector('.card-content');
-            const cardWidth = window.innerWidth / 2 > 600 ? window.innerWidth / 2 : window.innerWidth - 100;
-            let currentIndex = 0;
 
-            this.handleScrollNext = () => {
-                currentIndex = (currentIndex + 1) % cardsContainer.children.length;
-                this.scrollSlider();
-            };
-
-            this.handleScrollPrev = () => {
-                currentIndex = (currentIndex - 1 + cardsContainer.children.length) % cardsContainer.children.length;
-                this.scrollSlider();
-            };
-
-            this.scrollSlider = () => {
-                const scrollAmount = currentIndex * cardWidth;
-                cardsContainer.scrollLeft = scrollAmount;
-
-                if (currentIndex === cardsContainer.children.length - 1) {
-                    currentIndex = 0;
-                    cardsContainer.scrollLeft = 0;
-                }
-            };
-        },
-    },
-};
 </script>
 
 <template>
@@ -38,108 +7,13 @@ export default {
     <section class="mt-10 py-10">
 
         <div class="mt-5 grid grid-cols-2">
-            <h3 class="w-96 text-3xl uppercase ml-28 py-6">Featured Products</h3>
-            <div class="Carousel_Button flex justify-end">
-                <button id="prev" class="btn" @click="handleScrollPrev">
-                        <i class="fa-solid fa-angle-left"></i>
-                </button>
-
-                <button  id="next" class="btn" @click="handleScrollNext">
-                    <i class="fa-solid fa-chevron-right"></i>
-                </button>
-            </div>
+            <h3 class="w-96 text-3xl text-[#eb9843] font-medium uppercase text-center lg:ml-20 py-6">Featured Products</h3>
+            
         </div>
 
-        <div class="grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-2 gap-4 lg:grid-cols-4">
             <div class="col-span-2">
                 <img src="../assets/image/produc_470x.webp" class="lg:mx-auto" alt="">
-            </div>
-
-            <div class="col-span-2">
-                <div class="container">
-
-                    <div class="slider">
-
-                        <div class="card-content">
-                            <!-- Card -->
-                            <div class="card w-80">
-                                <div class="Package-card-img">
-                                    <img src="../assets/image/product1.webp" class="lg:h-96" alt="">
-                                </div>
-                                <div class="card-text">
-                                    
-                                </div>
-                            </div>
-                            <!-- Card End -->
-                                
-                            <!-- Card -->
-                            <div class="card">
-                        
-                                <div class="Package-card-img">
-                                    <img src="../assets/image/product2.webp" alt="">
-                                </div>
-                                <div class="card-text">
-                                   
-                                </div>
-                            </div>
-                                <!-- Card End -->
-                                
-                                <!-- Card -->
-                            <div class="card">
-                            
-                                <div class="Package-card-img">
-                                    <img src="../assets/image/product3.webp" alt="">
-                                </div>
-                                <div class="card-text">
-                                    
-                                </div>
-                            </div>
-                                <!-- Card End -->
-                                
-                                <!-- Card -->
-                            <div class="card">
-                            
-                                <div class="Package-card-img">
-                                    <img src="../assets/image/product4.webp" alt="">
-                                </div>
-                                <div class="card-text">
-                                    
-                                </div>
-                            </div>
-                                <!-- Card End -->
-                                
-                                <!-- Card -->
-                            <div class="card">
-                            
-                                <div class="Package-card-img">
-                                    <img src="../assets/image/product1.webp" alt="">
-
-                                </div>
-                                <div class="card-text">
-                                    
-                                </div>
-                            </div>
-                                <!-- Card End -->
-                                
-                                <!-- Card -->
-                            <div class="card">
-                            
-                                <div class="Package-card-img">
-                                    <img src="../assets/image/product2.webp" alt="">
-
-                                </div>
-                                <div class="card-text">
-                                    
-                                    
-                                </div>
-                            </div>
-                            <!-- Card End -->
-                            
-                        </div>
-                    
-                    </div>
-
-                </div>
             </div>
         </div>
 
@@ -163,12 +37,10 @@ export default {
   border: none;
   box-shadow: 0px 0px 0px 0px rgba(0,0,0, .08);
 }
-
 .card:hover {
   transform:translate(0, -10px);
   box-shadow: 0px 17px 35px 0px rgba(0,0,0,.07);
 }
-
 .card h4 {
   position:absolute;
   left: 0;
@@ -211,7 +83,6 @@ p {
     border-top-left-radius: 30px;
     border-top-right-radius: 30px;
     height: 310px;
-
 }
 .card-content {
   display:flex;
@@ -222,11 +93,9 @@ p {
   padding-right: 50px;
   scroll-behavior:smooth;
 }
-
 .card-content::-webkit-scrollbar {
    height:0px;
 }
-
 .card-content:after {
   content:'';
   display:block;
@@ -234,7 +103,6 @@ p {
   height:100px;
   position:relative;
 }
-
 .btn{
   min-width:50px;
   margin:auto 10px;
@@ -253,7 +121,6 @@ p {
   box-shadow: 0px 0px 0px 0px rgba(0,0,0,.08);
   transition: all .25s ease;
 }
-
 .btn:hover{
   box-shadow: 0px 17px 35px 0px rgba(0,0,0,.07);
 }
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -4,24 +4,24 @@
 
 <template>
 
-
-
     <div id="default-carousel" class="relative w-full" data-carousel="static">
         <!-- Carousel wrapper -->
-        <div class="relative h-full md:h-96 overflow-hidden">
+        <div class="relative h-56 overflow-hidden lg:h-[650px] md:h-96">
              <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <h2 class="absolute top-80 leading-3 ml-10 text-5xl font-semibold">We Provide The Best Quality Tools</h2>
-                <button type="button" class="absolute top-96 ml-10 hover:text-white border btn-outline-amber focus:outline-none font-medium rounded-lg text-base px-6 py-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Buy Products</button>
-                <img src="../assets/image/Slider2.jpeg" class="block w-full h-[650px] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="absolute z-10 lg:mt-64 p-8">
+                    <h2 class="text-6xl text-[#eb9843] font-normal">The Best Hardware Tools</h2>
+                    <p class="text-xl leading-8 text-white font-medium py-4">We only provide hardware that meets ANSI/BHMA <br> Grade-1 and Grade-2 standards</p>
+                </div>
+                <img src="../assets/image/Slider1.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="../assets/image/Slider1.jpeg" class="block w-full h-[650px] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="../assets/image/Slider2.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="../assets/image/Slider3.jpeg" class="block w-full h-[650px] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <img src="../assets/image/Slider3.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
         </div>
         <!-- Slider indicators -->
@@ -30,50 +30,10 @@
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
         </div>
-        <!-- Slider controls -->
-        <!-- <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button> -->
     </div>
-
 
 </template>
 
 <style scoped>
-h2{
-    color: #eb9843;
-}
-.relative{
-    height: 650px;
-}
-img{
-    width: 100%;
-    height: 750px;
-    background: no-repeat;
-    background-size: cover;
-}
-.btn-outline-amber{
-    background: transparent;
-    border: 1px solid #eb9843;
-    color: #eb9843;
-    transition: 0.6s all;
-}
-.btn-outline-amber:hover{
-    background: #eb9843;
-    border: transparent;
-    color: #fff;
-}
+
 </style>
