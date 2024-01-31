ff42031294407e400690bead0cb40bae3b8e1e74
ArifDoofazit <arif@doofazit.com>
2024-01-31 15:31:40 +0600

    bannerpage end

--- src/components/Banerpage.vue
+++ src/components/Banerpage.vue
@@ -5,12 +5,25 @@
 <template>
     <section class="">
         <div class="">
+            <h2 class="lg:text-3xl lg:ml-10 text-xl mt-5 font-semibold" style="color:#eb9843;">FEATURED</h2>
             <img src="../assets/banner-hardware.jpg" class="w-full lg:h-60 h-40 mt-10 opacity-30" alt="">
             <div class="container">
                 <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 gap-x-8 gap-y-4">
-                    <div class=""></div>
-                    <div class=""></div>
-                    <div class=""></div>
+                    <div class="z-10 h-72 w-72 shadow-md rounded-full lg:ml-28 ml-4 -mt-20 border-solid border-4 border-yellow-400">
+                    <img src="../assets/banner-1.jpg" class="h-72 w-72 shadow-md rounded-full" alt="">
+                    <h3 class="lg:ml-4 text-center text-2xl lg:font-semibold text-[#eb9843] mt-5">Installation</h3>
+                    <p class="text-center text-[#a6a5a4]">Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate</p>
+                    </div>
+                    <div class="z-10 h-72 w-72 shadow-md rounded-full lg:ml-28 ml-4 lg:-mt-28 mt-36 border-solid border-4 border-yellow-400">
+                        <img src="../assets/banner-2.jpg" class="h-72 w-72 shadow-md rounded-full" alt="">
+                        <h3 class="lg:ml-5 text-center text-2xl lg:font-semibold text-[#eb9843] mt-5">Products</h3>
+                        <p class="text-center text-[#a6a5a4]">Vivamus elementum semper nisi. Aenean vulputate.Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
+                    </div>
+                    <div class="z-10 h-72 w-72 shadow-md rounded-full lg:ml-28 ml-4 mt-36 lg:-mt-28 border-solid border-4 border-yellow-400">
+                        <img src="../assets/banner-4.jpg" class="h-72 w-72 shadow-md rounded-full" alt="">
+                        <h3 class="lg:ml-5 text-center text-2xl lg:font-semibold text-[#eb9843] mt-5">Sanitary</h3>
+                        <p class="text-center text-[#a6a5a4]">Aenean vulputate Integer tincidunt. Cras dapibus. Nullam dictum felis eu pede mollis pretium.Vivamus elementum semper nisi.</p>
+                    </div>
                 </div>
             </div>
         </div>
