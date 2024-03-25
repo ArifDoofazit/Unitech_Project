c8b9ec51b6829f10dfbc8ac5e970c1ab2e0a064f
ShFarhan767 <farhan@doofazit.com>
2024-03-25 15:25:32 +0600

    serviceInfo

--- src/components/ServiceInfo.vue
+++ src/components/ServiceInfo.vue
@@ -1,5 +1,5 @@
 <script setup>
-const serviceHeading = {
+const serviceInfo = {
     image: "/assets/About-4-mAf3Rufd.webp",
     smallQuote: "Unitech - Believe In Quality",
     title: "Where Quality Meets Affordability",
@@ -11,21 +11,20 @@ const serviceHeading = {
 <template>
     <div class="container mx-auto pb-10">
         <div class="grid lg:grid-cols-2 gap-5 group">
-            <div
-                class="Image w-full h-auto lg:rounded-2xl flex justify-center overflow-hidden">
+            <div class="Image w-full h-auto lg:rounded-2xl flex justify-center overflow-hidden">
                 <img class="w-full h-auto object-cover object-center rounded-2xl transition-all group-hover:scale-110 overflow-hidden"
-                    :src="serviceHeading.image" alt="">
+                    :src="serviceInfo.image" alt="">
             </div>
 
-            <div v-if="serviceHeading" class="text-info text-center">
-                <p class="lg:text-2xl font-light mt-12">{{ serviceHeading.smallQuote }}</p>
+            <div v-if="serviceInfo" class="text-info text-center">
+                <p class="lg:text-2xl font-light mt-12">{{ serviceInfo.smallQuote }}</p>
                 <h1 class="lg:text-5xl text-2xl font-medium leading-[60px] lg:mt-5 text-[#273270]">
-                    {{ serviceHeading.title }}
+                    {{ serviceInfo.title }}
                 </h1>
-                <p v-html="serviceHeading.description" class="lg:text-xl font-normal leading-7 mt-3 lg:mt-7 px-5"></p>
+                <p v-html="serviceInfo.description" class="lg:text-xl font-normal leading-7 mt-3 lg:mt-7 px-5"></p>
                 <button
                     class="lg:py-4 lg:px-10 px-7 py-3 bg-[#273270] text-white lg:text-2xl text-lg font-normal rounded-lg mt-10 transition-all hover:bg-[#eb9843]">
-                    {{ serviceHeading.buttonName }}
+                    {{ serviceInfo.buttonName }}
                 </button>
             </div>
         </div>
